08b8f9a3c443cf03f9f5678794804a5fc92b8281
Osuka-Andala <teresa.andala@gmail.com>
2015-08-03 13:00:19 +0300

    Initialized git and added files to the drawable

---
+++ app/src/main/java/com/moringaschool/chessthrones/Fragments.java
@@ -0,0 +1,11 @@
+package com.moringaschool.chessthrones;
+
+/**
+ * Created by osuka on 8/3/15.
+ */
+import ...import android.app.ListFragment;
+
+public class Fragments extends ListFragment {
+
+    public static int[] images =
+}
---
+++ app/src/main/java/com/moringaschool/chessthrones/Splash.java
@@ -0,0 +1,7 @@
+package com.moringaschool.chessthrones;
+
+/**
+ * Created by osuka on 8/3/15.
+ */
+public class Splash {
+}
